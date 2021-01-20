c39cbb743fd28fc1d85cc88208dc3d61807445ad
sezniv <sezniv@gmail.com>
2021-01-20 12:04:44 -0300

    Se agregan Modelo de Clase  orina, perfil_lipidico

--- app_med2/models.py
+++ app_med2/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Orina(models.Model):
+    id_orina = models.AutoField(primary_key=True,default=None)
+    fecha_orina = models.DateField()
+    nombre_orina = models.CharField(max_length=50, default="Examen de Orina")
+    color = models.CharField(max_length=25)
+    densidad = models.DecimalField(decimal_places=3)
+    glucosa = densidad = models.DecimalField(decimal_places=3)
+    cetonas = densidad = models.DecimalField(decimal_places=3)
+    urobilinogeno = densidad = models.DecimalField(decimal_places=3)
+    billirubina = models.DecimalField(decimal_places=3)
+    observaciones_orina = models.CharField(max_length=100)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=None)
+
+class PerfilLipidico(models.Model):
+    id_plipidico = models.AutoField(primary_key=True,default=None)
+    fecha_orina = models.DateField()
+    nombre_orina = models.CharField(max_length=50, default="Examen de Perfil Lipidico")
+    colesterol = models.DecimalField(decimal_places=3)
+    colesterol_ldl = models.DecimalField(decimal_places=3)
+    colesterol_hdl = models.DecimalField(decimal_places=3)
+    trigliceridos = models.DecimalField(decimal_places=3)
+    observacion_plipidico = models.CharField(max_length=100)
+
+
+
+
+
