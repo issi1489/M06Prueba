942530c6ade48ca50b10fcef370f0fd146b5f59c
issi1489 <issi1489@gmail.com>
2021-01-20 13:21:56 -0300

    Se añade labels a algunos atributos. Se homogeiniza formato de id, fk_rut, fecha, nombre examen, observaciones. Se añade required True a todos los campos no automáticos. Se homogeiniza decimal_places a 1.

--- app_med2/models.py
+++ app_med2/models.py
@@ -1,105 +1,130 @@
 from django.db import models
 
-class PerfilBioquimico(models.Model):
-    id_pbioquimico = models.AutoField(primary_key=True,default=None)
-    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE)
-    fecha_pbioquimico = models.DateTimeField(required=True)
-    nombre_pbioquimico = models.CharField(default="Perfil Bioquimico")
-    glucosa = models.DecimalField(decimal_places=3,required=True)
-    nitrogeno_ureico =models.DecimalField(decimal_places=3,required=True)
-    calcio = models.DecimalField(decimal_places=3,required=True)
-    fosforo = models.DecimalField(decimal_places=3,required=True)
-    proteinas_totales = models.DecimalField(decimal_places=3,required=True)
-    albumina =models.DecimalField(decimal_places=3,required=True)
-    colesterol_total = models.DecimalField(decimal_places=3,required=True)
-    acido_urico = models.DecimalField(decimal_places=3,required=True)
-    fosfatas_alcalinas = models.DecimalField(decimal_places=3,required=True)
-    bilirrubina_total = models.DecimalField(decimal_places=3,required=True)
-    ldh = models.DecimalField(decimal_places=3,required=True)
-    got_ast = models.DecimalField(decimal_places=3,required=True)
-    creatinina = models.DecimalField(decimal_places=3,required=True)
-    observaciones_pbioquimico = models.CharField(max_length=100, required=True, default="Parametros normales")
+class Usuario(models.Model):
+    #pk
+    rutUsuario = models.CharField(max_length=10, primary_key=True, required=True, default=None,label='Rut')
+    #atributos
+    nombre = models.CharField(required=True, max_length=45)
+    edad = models.IntegerField(required=True )
+    direccion = models.CharField(required=True, max_length=45)
+    staff = models.BooleanField(required=True)
+    usuario = models.CharField(required=True, max_length=10)
+    password = models.CharField(required=True, max_length=10)
 
 
 class Diagnostico(models.Model):
-    fecha_diagnostico = models.DateTimeField(required=True)
-    fk_rutUsuario = models.ForeignKey(Usuario, required=True, on_delete=models.CASCADE)
+    #pk
+    fecha_diagnostico = models.DateTimeField(label='Fecha', primary_key=True, required=True, default=None)
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
     diagnostico = models.CharField(max_length=100, required=True )
 
 
-class Usuario(models.Model):
+class PerfilBioquimico(models.Model):
 
-    rut = models.AutoField(primary_key=True, default=None, max_length=10)
-    nombre_paciente = models.CharField(max_length=45)
-    edad_paciente = models.IntegerField()
-    direccion_paciente = models.CharField(max_length=45)
-    staff = models.BooleanField()
-    usuario = models.CharField(max_length=10)
-    password = models.CharField(max_length=10)
+    #pk
+    id_pbioquimico = models.AutoField(primary_key=True,default=None, label="Id")
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
+    fecha_pbioquimico = models.DateTimeField(label='Fecha', required=True)
+    nombre_pbioquimico = models.CharField(default='Examen de Perfil Bioquimico')
+    glucosa = models.DecimalField(decimal_places=1, required=True)
+    nitrogeno_ureico = models.DecimalField(label='Nitrogeno ureico', decimal_places=1, required=True)
+    calcio = models.DecimalField(decimal_places=1, required=True)
+    fosforo = models.DecimalField(decimal_places=1, required=True)
+    proteinas_totales = models.DecimalField(label='Proteinas Totales', decimal_places=1, required=True)
+    albumina = models.DecimalField(decimal_places=1, required=True)
+    colesterol_total = models.DecimalField(label='Colesterol Total', decimal_places=1, required=True)
+    acido_urico = models.DecimalField(label='Acido urico', decimal_places=1, required=True)
+    fosfatas_alcalinas = models.DecimalField(label='Fosfatas alcalinas', decimal_places=1, required=True)
+    bilirrubina_total = models.DecimalField(label='Bilirrubina Total', decimal_places=1, required=True)
+    ldh = models.DecimalField(label='LDH',decimal_places=1, required=True)
+    got_ast = models.DecimalField(label='GOT/AST',decimal_places=1, required=True)
+    creatinina = models.DecimalField(decimal_places=1, required=True)
+    observaciones_pbioquimico = models.CharField(label='Observaciones', max_length=100, required=True, default="Parametros normales")
 
 
 class Hemograma(models.Model):
 
-    id_hemograma = models.AutoField(primary_key=True, default=None, max_length=10)
-    paciente_rut = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=None)
-    fecha_hemograma = models.DateField()
-    nombre_hemograma = models.CharField(default='Hemograma')
-    eritrocito = models.DecimalField(decimal_places=1)
-    leucocitos = models.DecimalField(decimal_places=1)
-    hemoglobina = models.DecimalField(decimal_places=1)
-    hematocrito = models.DecimalField(decimal_places=1)
-    vcm = models.DecimalField(label='VCM', decimal_places=1)
-    chcm = models.DecimalField(label='CHCM', decimal_places=1)
-    plaquetas = models.DecimalField(decimal_places=1)
-    observaciones_hemograma = models.CharField(max_length=100)
+    #pk
+    id_hemograma = models.AutoField(primary_key=True,default=None, label="Id")
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
+    fecha_hemograma = models.DateTimeField(label='Fecha', required=True)
+    nombre_hemograma = models.CharField(default='Examen de Hemograma')
+    eritrocito = models.DecimalField(decimal_places=1, required=True)
+    leucocitos = models.DecimalField(decimal_places=1, required=True)
+    hemoglobina = models.DecimalField(decimal_places=1, required=True)
+    hematocrito = models.DecimalField(decimal_places=1, required=True)
+    vcm = models.DecimalField(label='VCM', decimal_places=1, required=True)
+    chcm = models.DecimalField(label='CHCM', decimal_places=1, required=True)
+    plaquetas = models.DecimalField(decimal_places=1, required=True)
+    observaciones_hemograma = models.CharField(label='Observaciones', max_length=100, required=True, default="Parametros normales")
 
 
-class coagulacion(models.Model):
+class Coagulacion(models.Model):
 
-    paciente_rut = models.IntegerField()
-    id_coagulacion = models.AutoField(primary_key=True,default=None)
-    fecha = models.DateField()
-    nombre_coagulacion = models.CharField(max_length=50)
-    tiempo_protrombina = models.DecimalField(decimal_places=3)
-    porc_protrombina = models.DecimalField(decimal_places=3)
-    observaciones_coagulacion = models.CharField(max_length=50)
+    #pk
+    id_coagulacion = models.AutoField(primary_key=True,default=None, label="Id")
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
+    fecha_coagulacion = models.DateTimeField(label='Fecha', required=True)
+    nombre_coagulacion = models.CharField(default='Examen de Coagulacion')
+    tiempo_protrombina = models.DecimalField(label='Tiempo Protrombina', decimal_places=1, required=True)
+    porc_protrombina = models.DecimalField(label='Protrombina(%)', decimal_places=1, required=True)
+    observaciones_coagulacion = models.CharField(label='Observaciones', max_length=100, required=True, default="Parametros normales")
 
 
-class glicemia(models.Model):
+class Glicemia(models.Model):
 
-    paciente_rut = models.IntegerField()
-    id_glicemia = models.AutoField(primary_key=True,default=None)
-    fecha_glicemia = models.DateField()
-    nombre_glicemia = models.CharField(max_length=50)
-    glicemia_basal = models.DecimalField(decimal_places=3)
-    glicemia_120min= models.DecimalField(decimal_places=3)
-    observaciones_glicemia = models.CharField(max_length=50)
+    #pk
+    id_glicemia = models.AutoField(primary_key=True,default=None, label="Id")
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
+    fecha_glicemia = models.DateTimeField(label='Fecha', required=True)
+    nombre_glicemia = models.CharField(default='Examen de Glicemia')
+    glicemia_basal = models.DecimalField(label='Glicemia basal', decimal_places=1, required=True)
+    glicemia_120min= models.DecimalField(label='Glicemia 120min', decimal_places=1, required=True)
+    observaciones_glicemia = models.CharField(label='Observaciones', max_length=100, required=True, default="Parametros normales")
 
-    
+
 class Orina(models.Model):
-    id_orina = models.AutoField(primary_key=True,default=None)
-    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE)
-    fecha_orina = models.DateField()
-    nombre_examen = models.CharField(max_length=50, default="Examen de Orina")
-    color = models.CharField(max_length=25)
-    densidad = models.DecimalField(decimal_places=3)
-    glucosa = densidad = models.DecimalField(decimal_places=3)
-    cetonas = densidad = models.DecimalField(decimal_places=3)
-    urobilinogeno = densidad = models.DecimalField(decimal_places=3)
-    billirubina = models.DecimalField(decimal_places=3)
-    observaciones_orina = models.CharField(max_length=100)
+
+    #pk
+    id_orina = models.AutoField(primary_key=True,default=None, label="Id")
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
+    fecha_orina = models.DateTimeField(label='Fecha', required=True)
+    nombre_orina = models.CharField(default="Examen de Orina")
+    color = models.CharField(max_length=25, required=True)
+    densidad = models.DecimalField(decimal_places=1, required=True)
+    glucosa = models.DecimalField(decimal_places=1, required=True)
+    cetonas = models.DecimalField(decimal_places=1, required=True)
+    urobilinogeno = models.DecimalField(decimal_places=1, required=True)
+    billirubina = models.DecimalField(decimal_places=1, required=True)
+    observaciones_orina = models.CharField(label='Observaciones', max_length=100, required=True, default="Parametros normales")
 
 
 class PerfilLipidico(models.Model):
-    id_plipidico = models.AutoField(primary_key=True,default=None)
-    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE)
-    fecha_orina = models.DateField()
-    nombre_examen = models.CharField(max_length=50, default="Examen de Perfil Lipidico")
-    colesterol = models.DecimalField(decimal_places=3)
-    colesterol_ldl = models.DecimalField(decimal_places=3)
-    colesterol_hdl = models.DecimalField(decimal_places=3)
-    trigliceridos = models.DecimalField(decimal_places=3)
-    observacion_plipidico = models.CharField(max_length=100)
+
+    #pk
+    id_plipidico = models.AutoField(primary_key=True,default=None, label="Id")
+    #fk
+    fk_rutUsuario = models.ForeignKey(Usuario,required=True, on_delete=models.CASCADE, default=None,label='Rut')
+    #atributos
+    fecha_plipidico = models.DateTimeField(label='Fecha', required=True)
+    nombre_plipidico = models.CharField(default="Examen de Perfil Lipidico")
+    colesterol = models.DecimalField(decimal_places=1, required=True)
+    colesterol_ldl = models.DecimalField(label='LDL', decimal_places=1, required=True)
+    colesterol_hdl = models.DecimalField(label='HDL', decimal_places=1, required=True)
+    trigliceridos = models.DecimalField(decimal_places=1, required=True)
+    observacion_plipidico = models.CharField(label='Observaciones', max_length=100, required=True, default="Parametros normales")
 
 
 
