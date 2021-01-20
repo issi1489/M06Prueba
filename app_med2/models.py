ad4bfbe6620d20801b9f314fe8941a0f0391e518
walter bastias <walterpeter_13otmail.com>
2021-01-20 12:07:07 -0300

    subida de class de coagulo glicemia

--- app_med2/models.py
+++ app_med2/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class coagulacion(models.Model):
+
+    paciente_rut = models.IntegerField()
+    id_coagulacion = models.AutoField(primary_key=True,default=None)
+    fecha = models.DateField()
+    nombre_coagulacion = models.CharField(max_length=50)
+    tiempo_protrombina = models.DecimalField(decimal_places=3)
+    porc_protrombina = models.DecimalField(decimal_places=3)
+    observaciones_coagulacion = models.CharField(max_length=50)
+   
+   
+   
+class glicemia(models.Model):
+
+    paciente_rut = models.IntegerField()
+    id_glicemia = models.AutoField(primary_key=True,default=None)
+    fecha_glicemia = models.DateField()
+    nombre_glicemia = models.CharField(max_length=50)
+    glicemia_basal = models.DecimalField(decimal_places=3)
+    glicemia_120min= models.DecimalField(decimal_places=3)
+    observaciones_glicemia = models.CharField(max_length=50)
+
