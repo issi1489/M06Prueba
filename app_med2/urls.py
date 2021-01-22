32ebb8f2340a96920d5e7f60023c433b4ba93d61
issi1489 <issi1489@gmail.com>
2021-01-22 14:41:37 -0300

    Se eliminan algunos templates, se comenta las url de creacion de usuarios y diagnostico. Se actualiza views y se añade un template intermedio pra acceder a los listados. Se soluciona la visualización del listado de diagnostico pero sigue arrojando error (puede ser de forms.py). Se añade perfillipidico_list (funciona).

--- app_med2/urls.py
+++ app_med2/urls.py
@@ -4,20 +4,26 @@ from . import views
 app_name = 'app_med2'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', views.admin, name='admin'),
+    #path('admin/', views.admin, name='admin'),
     path('paciente/', views.paciente, name= 'paciente'),
+    # Vista admin
+    path('nuevosRegistros',views.nuevoRegistro, name='registroExamen'),
+    path('gestionarRegistros',views.gestionRegistro, name='registroExamen'),
 
+    #path crud diagnostico
+    path('listar_diagnosticos/', views.ListarDiagnostico.as_view(), name="listar_diagnosticos"),
+    path('<pk>/editar_diagnostico', views.UpdateDiagnostico.as_view(), name="editar_diagnostico"),
+    path('<pk>/eliminar_diagnostico', views.EliminarDiagnostico.as_view(), name="eliminar_diagnostico"),
     
-    path('admin', views.admin, name='admin'),
-    path('base', views.base, name='base'),
-    path('registroExamen',views.registroExamen, name='registroExamen'),
-    path('crear_usuario/', views.CrearUsuario.as_view(), name="crear_usuario"),
     path('listar_usuarios/', views.ListarUsuario.as_view(), name="listar_usuarios"),
     path('<pk>/editar_usuario', views.UpdateUsuario.as_view(), name="editar_usuario"),
     path('<pk>/eliminar_usuario', views.EliminarUsuario.as_view(), name="eliminar_usuario"),
-    #path crud diagnostico
-    path('crear_diagnostico/',views.CrearDiagnostico.as_view(),  name="crear_diagnostico"),
-    path('listar_diagnosticos/', views.ListarDiagnostico.as_view(), name="listar_diagnosticos"),
-    path('<pk>/editar_diagnostico', views.UpdateDiagnostico.as_view(), name="editar_diagnostico"),
-    path('<pk>/eliminar_diagnostico', views.EliminarDiagnostico.as_view(), name="eliminar_diagnostico")
+    #crud perfil lipidico
+    path('listar_perfillipidico/', views.ListarPerfilLipidico.as_view(), name="listar_usuarios"),
 ]
+
+'''
+Paths en desuso por funcion nuevoRegistro
+path('crear_usuario/', views.CrearUsuario.as_view(), name="crear_usuario"),
+path('crear_diagnostico/',views.CrearDiagnostico.as_view(),  name="crear_diagnostico"),
+'''
--- app_med2/views.py
+++ app_med2/views.py
@@ -7,71 +7,22 @@ from .forms import FormularioUsuario, UsuarioForms, DiagnosticoForms, PerfilBioq
 from .forms import CoagulacionForms, GlicemiaForms, OrinaForms, PerfilLipidicoForms
 from django.template.response import TemplateResponse
 
-class ListarUsuario(ListView):
-    model=Usuario
-    fields='__all__'
-    success_url=reverse_lazy('app_med2:listar_usuarios')
-
-class CrearUsuario(CreateView):
-    model=Usuario
-    template_name='app_med2/usuario_form.html'
-    fields='__all__'
-    success_url=reverse_lazy('app_med2:listar_usuarios')
-
-class UpdateUsuario(UpdateView):
-    model=Usuario
-    fields='__all__'
-    success_url=reverse_lazy('app_med2:listar_usuarios')
-
-class EliminarUsuario(DeleteView):
-    model= Usuario
-    fields='__all__'
-    success_url=reverse_lazy('app_med2:listar_usuarios')
-
-class ListarDiagnostico(ListView):
-    model=Diagnostico
-    fields='__all__'
-    success_url=reverse_lazy('app_med2:listar_diagnosticos')
-
-class CrearDiagnostico(CreateView):
-    model=Diagnostico
-    fields='_all_'
-    success_url=reverse_lazy('app_med2:listar_diagnosticos')
-    
-class UpdateDiagnostico(UpdateView):
-    model=Diagnostico
-    fields='_all_'
-    success_url=reverse_lazy('app_med2:editar_diagnosticos')
-
-class EliminarDiagnostico(DeleteView):
-    model=Diagnostico
-    fields='_all_'
-    success_url=reverse_lazy('app_med2:eliminar_diagnosticos')
-
-
-
-
-
-
-def base(request):
-
-    return render(request, 'app_med2/agregar/base.html')
-
-def admin(request):
-
-    return render(request, 'app_med2/admin.html')
-
-
+# Home 
 def home(request):
-
+    '''
+    Home page de el proyecto
+    '''
     return render(request, 'app_med2/home.html')
 
+# Vista admin
+def admin(request):
+    '''
+    (No funcional aún) Debe mostrar el perfil del usuario admin que ingresó
+    '''
+    return render(request, 'app_med2/admin.html')
 
-def paciente(request):
-    return render(request,'../templates/app_med2/paciente.html')
-
-
-def registroExamen(request):
+# Vista admin: Crear nuevos registros
+def nuevoRegistro(request):
     '''
     Función registroExamen:
         * Carga los formularios en el template reg_examen.html es este orden:
@@ -105,35 +56,35 @@ def registroExamen(request):
 
         if usuario_form.is_valid():
             usuario_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context) 
+            return render(request,'app_med2/registros_nuevos.html',context) 
 
         elif diagnostico_form.is_valid():
             diagnostico_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)     
+            return render(request,'app_med2/registros_nuevos.html',context)     
 
         elif lipidico_form.is_valid():
             lipidico_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)
+            return render(request,'app_med2/registros_nuevos.html',context)
 
         elif glicemia_form.is_valid():
             glicemia_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)
+            return render(request,'app_med2/registros_nuevos.html',context)
 
         elif coagulacion_form.is_valid():
             coagulacion_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)
+            return render(request,'app_med2/registros_nuevos.html',context)
 
         elif bioquimico_form.is_valid():
             bioquimico_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)
+            return render(request,'app_med2/registros_nuevos.html',context)
 
         elif orina_form.is_valid():
             orina_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)
+            return render(request,'app_med2/registros_nuevos.html',context)
 
         elif hemograma_form.is_valid():
             hemograma_form.save()
-            return render(request,'app_med2/agregar/reg_examen.html',context)   
+            return render(request,'app_med2/registros_nuevos.html',context)   
 
         else:
             context = {
@@ -161,3 +112,90 @@ def registroExamen(request):
         }
 
     return TemplateResponse(request, 'app_med2/registros_nuevos.html', context)
+
+# Vista admin: Template intermedio con links a listas
+def gestionRegistro(request):
+    '''
+    Función para visualizar un template intermedio que permite ingresar
+    a las distintas tablas con datos de usuario, diagnostico y exámenes.
+    '''
+    return render(request,'../templates/app_med2/registros_gestion.html')
+
+
+# Vista paciente
+def paciente(request):
+    return render(request,'../templates/app_med2/paciente.html')
+
+#----------------------------------- CRUDS ---------------------------------
+
+# CRUD USUARIO
+class ListarUsuario(ListView):
+    model=Usuario
+    fields='__all__'
+    success_url=reverse_lazy('app_med2:listar_usuarios')
+
+class UpdateUsuario(UpdateView):
+    model=Usuario
+    fields='__all__'
+    success_url=reverse_lazy('app_med2:listar_usuarios')
+
+class EliminarUsuario(DeleteView):
+    model= Usuario
+    fields='__all__'
+    success_url=reverse_lazy('app_med2:listar_usuarios')
+
+# CRUD DIAGNOSTICO
+class ListarDiagnostico(ListView):
+    model=Diagnostico
+    fields='__all__'
+    success_url=reverse_lazy('app_med2:listar_diagnosticos')
+    
+class UpdateDiagnostico(UpdateView):
+    model=Diagnostico
+    fields='_all_'
+    success_url=reverse_lazy('app_med2:editar_diagnosticos')
+
+class EliminarDiagnostico(DeleteView):
+    model=Diagnostico
+    fields='_all_'
+    success_url=reverse_lazy('app_med2:eliminar_diagnosticos')
+
+# CRUD PERFIL LIPIDICO
+class ListarPerfilLipidico(ListView):
+    model=PerfilLipidico
+    fields='__all__'
+    success_url=reverse_lazy('app_med2:listar_perfillipidico')
+    
+class UpdatePerfilLipidico(UpdateView):
+    model=PerfilLipidico
+    fields='_all_'
+    success_url=reverse_lazy('app_med2:editar_perfillipidico')
+
+class EliminarPerfilLipidico(DeleteView):
+    model=PerfilLipidico
+    fields='_all_'
+    success_url=reverse_lazy('app_med2:eliminar_perfillipidico')
+
+
+
+
+
+
+
+# ----------- CODIGO QUE DEBE ELIMINARSE AL CONFIRMAR QUE NO SE USARÁ -------------
+'''
+CRUD: CREAR USUARIO Y DIAGNOSTICO
+class CrearUsuario(CreateView):
+    model=Usuario
+    template_name='app_med2/usuario_form.html'
+    fields='__all__'
+    success_url=reverse_lazy('app_med2:listar_usuarios')
+
+
+class CrearDiagnostico(CreateView):
+    model=Diagnostico
+    fields='_all_'
+    success_url=reverse_lazy('app_med2:listar_diagnosticos')
+
+
+'''
